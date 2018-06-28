bd43d95accdf42d1386d5b6330bb073ed9af6c92
erikwiklander <erik.wiklander@wiklandia.com>
2018-06-28 20:43:19 +0200

    Fixed sonar code smells

--- src/main/java/io/wiklandia/flex/controller/FlexPage.java
+++ src/main/java/io/wiklandia/flex/controller/FlexPage.java
@@ -1,28 +1,31 @@
 package io.wiklandia.flex.controller;
 
-import lombok.Data;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 
-import java.util.List;
+import lombok.Data;
 
 @Data
 public class FlexPage<T> {
 
-  private List<T> content;
-  private PageInfo page = new PageInfo();
+	private List<T> content;
+	private PageInfo page = new PageInfo();
 
-  public FlexPage(Page<T> p) {
-    this.content = p.getContent();
-    this.page.number = p.getNumber();
-    this.page.size = p.getSize();
-    this.page.totalElements = p.getTotalElements();
-    this.page.totalPages = p.getTotalPages();
-  }
+	public FlexPage(Page<T> p) {
+		this.content = p.getContent();
+		this.page.number = p.getNumber();
+		this.page.size = p.getSize();
+		this.page.totalElements = p.getTotalElements();
+		this.page.totalPages = p.getTotalPages();
+	}
 
-  @Data
-  static class PageInfo {
-    int number, size, totalPages;
-    long totalElements;
-  }
+	@Data
+	static class PageInfo {
+		int number;
+		int size;
+		int totalPages;
+		long totalElements;
+	}
 
 }
--- src/test/java/io/wiklandia/flex/model/AttributeTypeTest.java
+++ src/test/java/io/wiklandia/flex/model/AttributeTypeTest.java
@@ -1,35 +1,37 @@
 package io.wiklandia.flex.model;
 
-import org.junit.Test;
-
-import static io.wiklandia.flex.model.AttributeType.*;
+import static io.wiklandia.flex.model.AttributeType.DATE;
+import static io.wiklandia.flex.model.AttributeType.DATE_TIME;
+import static io.wiklandia.flex.model.AttributeType.DECIMAL;
+import static io.wiklandia.flex.model.AttributeType.LONG;
+import static io.wiklandia.flex.model.AttributeType.TEXT;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class AttributeTypeTest {
 
+	@Test
+	public void testNumberOfValueTypes() {
+		assertEquals("When value types are added, please update tests in this class", 5, AttributeType.values().length);
+	}
 
-  @Test
-  public void testNumberOfValueTypes() {
-    assertEquals("When value types are added, please update tests in this class",
-      5, AttributeType.values().length);
-  }
+	@Test
+	public void testColName() {
+		assertEquals("Correct db col", "text_value", TEXT.getDbCol());
+		assertEquals("Correct db col", "decimal_value", DECIMAL.getDbCol());
+		assertEquals("Correct db col", "long_value", LONG.getDbCol());
+		assertEquals("Correct db col", "date_value", DATE.getDbCol());
+		assertEquals("Correct db col", "date_time_value", DATE_TIME.getDbCol());
+	}
 
-  @Test
-  public void testColName() {
-    assertEquals("Correct db col", TEXT.getDbCol(), "text_value");
-    assertEquals("Correct db col", DECIMAL.getDbCol(), "decimal_value");
-    assertEquals("Correct db col", LONG.getDbCol(), "long_value");
-    assertEquals("Correct db col", DATE.getDbCol(), "date_value");
-    assertEquals("Correct db col", DATE_TIME.getDbCol(), "date_time_value");
-  }
-
-  @Test
-  public void testFieldName() {
-    assertEquals("Correct db col", TEXT.getField(), "textValue");
-    assertEquals("Correct db col", DECIMAL.getField(), "decimalValue");
-    assertEquals("Correct db col", LONG.getField(), "longValue");
-    assertEquals("Correct db col", DATE.getField(), "dateValue");
-    assertEquals("Correct db col", DATE_TIME.getField(), "dateTimeValue");
-  }
+	@Test
+	public void testFieldName() {
+		assertEquals("Correct db col", "textValue", TEXT.getField());
+		assertEquals("Correct db col", "decimalValue", DECIMAL.getField());
+		assertEquals("Correct db col", "longValue", LONG.getField());
+		assertEquals("Correct db col", "dateValue", DATE.getField());
+		assertEquals("Correct db col", "dateTimeValue", DATE_TIME.getField());
+	}
 
 }
